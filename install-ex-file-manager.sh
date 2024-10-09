5441d266b022391e1d7ef8b500cabc07e8abdc88
Nizpew <144165519+nizpew@users.noreply.github.com>
2024-10-08 22:18:50 -0400

    fixtypo

--- install-ex-file-manager.sh
+++ install-ex-file-manager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y curl sudo fzf st
+sudo apt update && sudo apt install -y curl sudo fzf stterm
 
 sudo cp ~/exfilemanager/exfilemanager  /usr/local/bin/
 
