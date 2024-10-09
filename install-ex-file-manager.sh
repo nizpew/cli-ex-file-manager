ac8ad346cf1bdb1cb694fd67761b74efbc11bcc8
Nizpew <144165519+nizpew@users.noreply.github.com>
2024-10-09 19:09:10 -0400

    Update install-ex-file-manager.sh

--- install-ex-file-manager.sh
+++ install-ex-file-manager.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 sudo apt update && sudo apt install -y curl sudo fzf stterm
 
 sudo cp ~/exfilemanager/exfilemanagercli /usr/local/bin/
